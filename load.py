e0d6a7eb6a3f9de8bdb9f896c703b2630ce9b504
yuonoda <yuonoda95@gmail.com>
2023-08-16 22:15:40 +0900

    feat: ロードを関数にした

--- load.py
+++ load.py
@@ -4,37 +4,38 @@ from setting import session
 import os
 from dotenv import load_dotenv
 
-# 変数の設定
+def load(SurveyNumber: int, csvPath: str) -> None:
+    # 対象のレコードを事前に削除
+    session.query(Answer).filter(Answer.SurveyNumber == SurveyNumber).delete(synchronize_session='fetch')
+
+    # CSVを読み込み
+    data = []
+    with open(csvPath, 'r') as file:
+        reader = csv.DictReader(file)
+        i = 0
+        for row in reader:
+            reader = csv.DictReader(file)
+
+            # 回答モデルにマッピング
+            data.append({
+                "SurveyNumber": SurveyNumber,
+                "AnswerKey": row['key'],
+                "UserID": row['pkey'],
+                "Age": row['y22_q2'],
+                "Gender": row['y22_q1'],
+                "EducationalAttainment":row['y22_q5'],
+                "MainJobIncome":  row['y22_q100_1'] if row['y22_q100_1'] != '' else 0,
+            })
+
+            # 1000件ごとにバルクインサート
+            if i % 1000 == 0:
+                session.bulk_insert_mappings(Answer, data)
+                session.commit()
+                data = []
+            i += 1
+
+        print(i)
+
 load_dotenv()
 csvPath = os.getenv("CSV_PATH")
-SurveyNumber = 1523
-
-# 対象のレコードを事前に削除
-session.query(Answer).filter(Answer.SurveyNumber == SurveyNumber).delete(synchronize_session='fetch')
-
-data = []
-with open(csvPath, 'r') as file:
-    reader = csv.DictReader(file)
-    i = 0
-    for row in reader:
-        reader = csv.DictReader(file)
-
-        # 回答モデルにマッピング
-        data.append({
-            "SurveyNumber": SurveyNumber,
-            "AnswerKey": row['key'],
-            "UserID": row['pkey'],
-            "Age": row['y22_q2'],
-            "Gender": row['y22_q1'],
-            "EducationalAttainment":row['y22_q5'],
-            "MainJobIncome":  row['y22_q100_1'] if row['y22_q100_1'] != '' else 0,
-        })
-
-        # 1000件ごとにバルクインサート
-        if i % 1000 == 0:
-            session.bulk_insert_mappings(Answer, data)
-            session.commit()
-            data = []
-        i += 1
-
-    print(i)
+load(1523, csvPath)
