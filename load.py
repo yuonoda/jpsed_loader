ab94d1a7c1e80f4223d623c8a473a91613481e8b
yuonoda <yuonoda95@gmail.com>
2023-08-17 18:00:44 +0900

    feat: 変数とサーベイを増やした

--- load.py
+++ load.py
@@ -1,5 +1,5 @@
 import csv
-from answer import Answer
+from migrate import Answer
 from setting import session
 import os
 from dotenv import load_dotenv
@@ -7,40 +7,62 @@ from dotenv import load_dotenv
 
 class Loader:
     KEY_DICT_BY_SURVEY = {
+        1429: {
+            "age": "y21_q2",
+            "gender": "y21_q1",
+            "educational_attainment": "y21_q5",
+            "main_job_income": "y21_q100_1",
+            "occupation": "y21_q32",
+            "degree": "y21_q6",
+            "self_learning": "y21_q69",
+        },
         1523: {
-            "Age": "y22_q2",
-            "Gender": "y22_q1",
-            "EducationalAttainment": "y22_q5",
-            "MainJobIncome": "y22_q100_1"
+            "age": "y22_q2",
+            "gender": "y22_q1",
+            "educational_attainment": "y22_q5",
+            "main_job_income": "y22_q100_1",
+            "occupation": "y22_q32",
+            "degree": "y22_q6",
+            "self_learning": "y22_q67",
         },
     }
 
     def __init__(self):
         load_dotenv()
-        self.csvPath = os.getenv("CSV_PATH")
+        self.csvDir = os.getenv("CSV_DIR")
+
+    def load_all(self) -> None:
+        for survey_number in self.KEY_DICT_BY_SURVEY.keys():
+            self.load(survey_number)
 
     def load(self, survey_number: int) -> None:
-        # 対象のレコードを事前に削除
-        session.query(Answer).filter(Answer.SurveyNumber == survey_number).delete(synchronize_session='fetch')
+
+        # その調査のレコードを事前に削除
+        session.query(Answer).filter(Answer.survey_number == survey_number).delete(synchronize_session='fetch')
 
         # キーの辞書を取得
         key_dict = self.KEY_DICT_BY_SURVEY[survey_number]
 
         # CSVを読み込み
         data = []
-        with open(self.csvPath, 'r') as file:
+        csvPath = self.csvDir + str(survey_number) + ".csv"
+        with open(csvPath, 'r') as file:
             reader = csv.DictReader(file)
             i = 0
             for row in reader:
+
                 # 回答モデルにマッピング
                 data.append({
-                    "SurveyNumber": survey_number,
-                    "AnswerKey": row['key'],
-                    "UserID": row['pkey'],
-                    "Age": row[key_dict['Age']],
-                    "Gender": row[key_dict['Gender']],
-                    "EducationalAttainment": row[key_dict['EducationalAttainment']],
-                    "MainJobIncome": row[key_dict["MainJobIncome"]] if row[key_dict['MainJobIncome']] != '' else 0,
+                    "survey_number": survey_number,
+                    "answer_key": row['key'],
+                    "user_ID": row['pkey'],
+                    "age": row[key_dict['age']],
+                    "gender": row[key_dict['gender']],
+                    "educational_attainment": row[key_dict['educational_attainment']],
+                    "main_job_income": row[key_dict["main_job_income"]] if row[key_dict['main_job_income']] != '' else 0,
+                    "occupation": row[key_dict['occupation']] if row[key_dict['occupation']] != '' else None,
+                    "degree": row[key_dict['degree']] if row[key_dict['degree']] != '' else None,
+                    "self_learning": row[key_dict['self_learning']] == '1'
                 })
 
                 # 1000件ごとにバルクインサート
@@ -54,4 +76,4 @@ class Loader:
 
 
 loader = Loader()
-loader.load(1523)
+loader.load_all()
